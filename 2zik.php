78d8d1a0ddfeac8986ddbad321be7629fe456dba
Maria <rainbowmelox@gmail.com>
2025-02-23 20:18:04 +0300

    obnov

--- 2zik.php
+++ 2zik.php
@@ -9,7 +9,7 @@ for ($i = 2; $i <= $n; $i++) {
     $isPrime = true;
 
     for ($j = 2; $j < $i; $j++) {
-        if ($i % $j == 0) {
+        if ($i % $j === 0) {
             $isPrime = false; 
             break;
         }
